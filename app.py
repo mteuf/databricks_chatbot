43936c3834bb917065233c5a40d08ec4d80924aa
mteuf <mteuf@acehardware.com>
2025-06-03 15:55:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-st.set_page_config(page_title="Field Staff Chatbot v3")
+st.set_page_config(page_title="Field Staff Chatbot v4")
 st.title("Field Staff Chatbot v3")
 
 # Initialize message history
